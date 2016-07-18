a7250231f904e5327b939b0b3b9b4146a8b56650
Basilliy <z.kon2009@gmail.com>
2016-07-19 01:31:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
  * Date: 18.07.2016
  * Time: 10:56
  */
- $verify_token = "hi"; // Verify token 
-if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
-echo $_REQUEST['hub_challenge']; 
-}
+ //$verify_token = "hi"; // Verify token 
+//if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
+//echo $_REQUEST['hub_challenge']; 
+//}
+
+file_put_contents('fb.txt',file_get_contents('php://input'));
+
