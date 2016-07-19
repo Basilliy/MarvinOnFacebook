f43ee620ed5694f32825c3d20511206e7286b550
Basilliy <z.kon2009@gmail.com>
2016-07-19 10:58:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,25 @@
 //echo $_REQUEST['hub_challenge']; 
 //}
 
-file_put_contents('fb.txt',file_get_contents('php://input'));
+//file_put_contents('fb.txt',file_get_contents('php://input'));
+$fb = json_decode(file_get_contents('php://input'),true);
+$id = $fb->entry[0]->messaging[0]->sender->id;
+$token = 'EAAXK3CoMH0QBAM3gZClSKzVcMLnL4uVvvUJG7wQaifTjgN65T2F8SmftMLJyD3uZCky02NA0bLjzEdfzhYc3TUY4HO8WkyqMZBZBdXD0P7BQlzge9CwZAZCZCDAybdGSyyoKJqRF1Rqj5nE723f5v8TqIawkWph7zeJdXxkYqUTnZCz7FHLLY59O';
+
+$data = array(
+      'recipient' => array('id' => $id ),
+      'message' => array('text' => 'russik')
+ );
+
+$options = array(
+          'http'=>array(
+             'method' => 'POST',
+             'content' => json_encode($data),
+             'header' => "Content-Type: application/json\n"
+             )
+ );
+
+$contex = stream_context_create('$options');
+
+file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
 
