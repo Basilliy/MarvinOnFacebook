117661cdca73d6216e08bc1b77e3d432d25425cf
Basilliy <z.kon2009@gmail.com>
2016-07-19 11:56:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,10 @@ $fb = file_get_contents("fb.txt");
 $fb = json_decode($fb);
 $id = $fb->entry[0]->messaging[0]->sender->id;
 $token = "EAAXK3CoMH0QBAM3gZClSKzVcMLnL4uVvvUJG7wQaifTjgN65T2F8SmftMLJyD3uZCky02NA0bLjzEdfzhYc3TUY4HO8WkyqMZBZBdXD0P7BQlzge9CwZAZCZCDAybdGSyyoKJqRF1Rqj5nE723f5v8TqIawkWph7zeJdXxkYqUTnZCz7FHLLY59O";
-
+$fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
 $data = array(
       'recipient' => array('id' => "$id" ),
-      'message' => array('text' => "russik")
+      'message' => array('text' => "$fuck")
  );
 
 $options = array(
