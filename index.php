b1cbcd5870e8eeb46879b5bde4f01f5f4fbbc91b
Basilliy <z.kon2009@gmail.com>
2016-07-19 12:53:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $fb = json_decode($fb);
 //$fb = json_decode(file_get_contents("php://input"), true);
 $id = $fb->entry[0]->messaging[0]->sender->id;
 $reid = $fb->entry[0]->messaging[0]->recipient->id;
+$message = $fb->entry[0]->messaging[0]-message->text;
 $token = "EAAXK3CoMH0QBAM3gZClSKzVcMLnL4uVvvUJG7wQaifTjgN65T2F8SmftMLJyD3uZCky02NA0bLjzEdfzhYc3TUY4HO8WkyqMZBZBdXD0P7BQlzge9CwZAZCZCDAybdGSyyoKJqRF1Rqj5nE723f5v8TqIawkWph7zeJdXxkYqUTnZCz7FHLLY59O";
 $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
 $data = array(
