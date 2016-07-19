48492f36fb65653ee7c99dca1a0fdd5cf21860c4
Basilliy <z.kon2009@gmail.com>
2016-07-19 15:00:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ $options = array(
 
 $context = stream_context_create($options);
 
-file_get_contents("https://graph.facebook.com/v2.7/me/messages?access_token=$token",false, $context);
+file_get_contents("https://graph.facebook.com/v2.7/me/messages?access_token=$token",true, $context);
 
 
 
