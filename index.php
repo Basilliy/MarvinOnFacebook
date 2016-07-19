9e298fe86130de4b6abda0dfead791133b3560aa
Basilliy <z.kon2009@gmail.com>
2016-07-19 12:29:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,12 @@
 //echo $_REQUEST['hub_challenge']; 
 //}
 
-file_put_contents("fb.txt",file_get_contents("php://input"));
-$fb = file_get_contents("fb.txt");
-$fb = json_decode($fb);
+//file_put_contents("fb.txt",file_get_contents("php://input"));
+//$fb = file_get_contents("fb.txt");
+//$fb = json_decode($fb);
+$fb = json_decode(file_get_contents("php://input"), true);
 $id = $fb->entry[0]->messaging[0]->sender->id;
+$reid = $fb->entry[0]->messaging[0]->recipient->id;
 $token = "EAAXK3CoMH0QBAM3gZClSKzVcMLnL4uVvvUJG7wQaifTjgN65T2F8SmftMLJyD3uZCky02NA0bLjzEdfzhYc3TUY4HO8WkyqMZBZBdXD0P7BQlzge9CwZAZCZCDAybdGSyyoKJqRF1Rqj5nE723f5v8TqIawkWph7zeJdXxkYqUTnZCz7FHLLY59O";
 $fuck = file_get_contents('https://evilinsult.com/generate_insult.php?lang=en');
 $data = array(
@@ -30,8 +32,6 @@ $options = array(
  );
 
 $context = stream_context_create($options);
-$go = true;
-if($go==true){
+if($id!=$reid){
 file_get_contents("https://graph.facebook.com/v2.7/me/messages?access_token=$token",false, $context);
-$go = false;
 }
